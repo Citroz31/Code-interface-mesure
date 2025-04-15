94b6530331daf26fc7af6fe46da4993e4f9e1816
Citroz31 <valentin.deremaux@gmail.com>
2025-04-15 11:04:15 +0200

    Update function.py

--- function.py
+++
@@ -1,9 +0,0 @@
-import skrf as rf
-
-def load_s2p(filepath):
-    try:
-        network=rf.Network(filepath)
-        return network
-    except Exception as e:
-        print(f"Erreur lors du chargement du fichier .s2p : {e}")
-        return None
